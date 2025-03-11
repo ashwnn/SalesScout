ad80054343af701988c45e2608be7b1f41313a7d
Ashwin <69374625+xxiz@users.noreply.github.com>
2025-03-11 14:20:07 -0700

    disable registration

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { register, login, getProfile } from '../controllers/userController';
+import { login, getProfile } from '../controllers/userController';
 import { protect } from '../middleware/auth';
 
 const router = express.Router();
 
 // Public routes
-router.post('/register', register);
+
+// router.post('/register', register);
 router.post('/login', login);
 
 // Protected routes
